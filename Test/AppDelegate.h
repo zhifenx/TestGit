9c044cdc5e97d5fe6bddad49ce6f85c9fd2e8cff
zhangzhifeng <837856614@qq.com>
2017-08-01 14:10:28 +0800

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by zhifenx on 2017/8/1.
+//  Copyright © 2017年 zhifenx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
